3d37e3c0cfc737bbace8c93380981e8cf34c5a32
Justin Lange <well1912@gmail.com>
2016-09-03 18:23:40 -0700

    Add page for API results

--- herokutest/views/default.py
+++ herokutest/views/default.py
@@ -3,9 +3,13 @@ import os
 from pyramid.response import Response
 from pyramid.view import view_config
 import googleapiclient.discovery as discovery
+from pprint import pformat
 
 
-@view_config(route_name='home')
+SEATTLE_UNIVERSITY_ADDRESS = '901 12th Avenue Seattle WA'
+
+
+@view_config(route_name='home', renderer='templates/test_api.jinja2')
 def my_view(request):
     civicinfo_service = discovery.build(
         'civicinfo',
@@ -13,5 +17,8 @@ def my_view(request):
         developerKey=os.environ.get('APIKEY')
     )
     elections = civicinfo_service.elections()
-    info_query = elections.voterInfoQuery(address=request.params['address'])
-    return Response(str(info_query.execute()))
+    info_query = elections.voterInfoQuery(
+        address=request.params.get('address', SEATTLE_UNIVERSITY_ADDRESS)
+    )
+    formatted_response = pformat(info_query.execute())
+    return {'response': formatted_response}
