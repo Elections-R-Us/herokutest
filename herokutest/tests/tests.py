7bc04e80b5571cf925c89e87b01cbc9ba122d17a
Justin Lange <well1912@gmail.com>
2016-09-03 18:57:16 -0700

    Cover 404 and bad address in tests

--- herokutest/tests/tests.py
+++ herokutest/tests/tests.py
@@ -1,2 +1,14 @@
-def test_root(testapp):
-    assert testapp.get('/'), str
+def test_layout_h1(testapp):
+    assert testapp.get('/').html.find('h1')
+
+
+def test_layout_response(testapp):
+    assert testapp.get('/').html.find(attrs={'id': 'response'})
+
+
+def test_layout_404(testapp):
+    assert testapp.get('/heck', status=404)
+
+
+def test_layout_bad_address(testapp):
+    assert 'bad address' in testapp.get('/?address=heck')
